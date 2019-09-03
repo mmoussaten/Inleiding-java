63a9b7e2219e6e898f1cb8e80080d8d1e31ecdba
marwan <marwan0320@live.nl>
2019-09-03 13:49:46 +0200

    h2

---
+++ src/h02/h2.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+public class h2 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color. blue );
+        g.drawString("Marwan ", 50, 60);
+        g.setColor(Color. red );
+        g.drawString("Moussaten", 50, 80);
+    }
+}
