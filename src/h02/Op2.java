1d3b46262b4acefb5af30ff483e5606a9980291c
marwan <marwan0320@live.nl>
2020-09-09 10:16:41 +0200

    eerste versie

--- src/h02/Op2.java
+++ src/h02/Op2.java
@@ -5,14 +5,14 @@ import java.awt.*;
 
 public class Op2 extends Applet {
 
-    public void init() {
+public void init() {
         setBackground(Color.white);
-    }
+        }
 
-    public void paint(Graphics g) {
+public void paint(Graphics g) {
         g.setColor(Color.blue);
         g.drawString("Marwan ", 50, 60);
         g.setColor(Color.red);
         g.drawString("Moussaten", 50, 80);
-    }
-}
+        }
+        }
