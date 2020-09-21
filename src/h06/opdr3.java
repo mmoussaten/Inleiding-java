7fc5f97a509d9329947e47553ef90a0f641edc99
marwan <marwan0320@live.nl>
2020-09-21 13:05:51 +0200

    java update

--- src/h06/opdr3.java
+++ src/h06/opdr3.java
@@ -1,4 +1,32 @@
 package h06;
 
-public class opdr3 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class opdr3 extends Applet {
+    int getal;
+    int getal2;
+    int res;
+
+
+    public void init() {
+        setSize(400, 400);
+
+        getal=80;
+        getal2 =100;
+
+        res= getal * 100 / getal2;
+
+
+    }
+
+
+    public void paint(Graphics g) {
+
+        //de rekensom laten aan te tonen op de applet
+        g.drawString(String.valueOf(res),110,310);
+
+
+
+    }
 }
